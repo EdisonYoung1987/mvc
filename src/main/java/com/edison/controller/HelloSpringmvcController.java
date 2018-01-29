2e6878f3e6c72e215d2cd755d38c4907cf96b9dd
EdisonYoung1987 <edison_young@aliyun.com>
2018-01-29 13:23:01 +0800

    ModelAndView增加LIST和MAP的传值以及jsp的显示过程

--- src/main/java/com/edison/controller/HelloSpringmvcController.java
+++ src/main/java/com/edison/controller/HelloSpringmvcController.java
@@ -1,5 +1,12 @@
 package com.edison.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +23,25 @@ public class HelloSpringmvcController {
         public ModelAndView showMessage( String name) {
         ModelAndView mv = new ModelAndView("hellospring");//指定视图
         
-        //向视图中添加所要展示或使用的内容，将在页面中使用
+        //向视图中添加所要展示或使用的内容，将在页面中使用,添加map对象
         mv.addObject("message", message);
         mv.addObject("name", name);
+        
+        //添加list
+        List<String> list=new ArrayList<String>();
+        SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+        list.add("开发者："+"Edison");
+        list.add("日期："+ df.format(new Date()));
+        list.add("联系方式："+"272882055");
+        mv.addObject("INFOLIST",list);
+        
+        //添加Map
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("Title:","Spring MVC LEARNING");
+        map.put("CHAPTER", "17TH");
+        map.put("Msg", "加油");
+        mv.addObject("STUDYMAP",map);
+        
         return mv;
     }
 }
