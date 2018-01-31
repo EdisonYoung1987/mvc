04a8821d545ee089cbb8ec83876eaa8fb037a5fa
EdisonYoung1987 <edison_young@aliyun.com>
2018-01-31 13:18:03 +0800

    针对用户注册，增加@ModelAttribute("user")方式，将表单内容以对象的形式传递给注册方法

--- src/main/java/com/edison/controller/UserController.java
+++ src/main/java/com/edison/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +20,17 @@ public class UserController {
 	@Autowired 
 	private User user;
 	String message = "Welcome to Spring MVC!";
-	 
+	
+	@ModelAttribute("user")
+	public User getUser(){
+		User user=new User();
+		return user;
+	}
+	
     @RequestMapping("/register")
     public String register() {
     	//返回注册界面jsp
+    	System.out.println("Func register() do");
         return "user/register";
     }
     
@@ -38,11 +46,14 @@ public class UserController {
         return "user/delete";
     }
     
-    @RequestMapping(method=RequestMethod.POST)
-    public ModelAndView create(User user,@RequestBody String req) {
+    @RequestMapping(value="/register.do",method=RequestMethod.POST)
+    public ModelAndView create(@ModelAttribute("user") User user,@RequestBody String req) {//必须有前面的被注解的getUser方法才行。
+    	System.out.println("Func create() do");
+
     	//返回注册成功界面jsp
     	  ModelAndView mv = new ModelAndView("user/success");//指定视图
-          
+          System.out.println(user.getUserName());
+          System.out.println(user.getPassword());
           //向视图中添加所要展示或使用的内容，将在页面中使用
           mv.addObject("message", "您好,"+user.getUserName()+"! 欢迎加入数据库！");
           
--- src/main/webapp/WEB-INF/views/user/register.jsp
+++ src/main/webapp/WEB-INF/views/user/register.jsp
@@ -7,8 +7,7 @@
 </head>
 <body>
 <!--  
-  <form method="post" action="/user.html">
- -->
+  
   <form method="post" action="<c:url value="/user.html"/>">
     <table>
 	    <tr>
@@ -23,5 +22,23 @@
 	    </tr>	    
     </table>
   </form>
+  -->
+  <h2>新用户注册</h2>
+     <form:form method="POST" modelAttribute="user" action="register.do" >  
+        <table>  
+                <tr><!-- tr表示一行，td表示一格 -->  
+                    <td>名称</td>  
+                    <!-- form:input 会被渲染成HTML的<input>标签     其中path属性会将input标签中value的值设置为模型中的path属性对应的值 -->  
+                    <td><form:input type="text" path="userName"/></td>
+                </tr>
+                <tr>
+                    <td>密码</td>   
+                    <td><form:password  path="password"/></td>  
+                </tr>   
+                </tr>
+	     			<td colspan="2"><input type="submit" name="提交ddd" value="提交"/></td>
+	    		</tr>
+        </table>  
+    </form:form>  
 </body>
 </html>
