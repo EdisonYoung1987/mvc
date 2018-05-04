fb5ebd25493d7e28946d9574cc6de0f7bb1f951d
edisonyoung1987 <edison_young@aliyun.com>
2018-05-04 16:27:28 +0800

    新增一个ThreadPoolExecutor的测试类-未完成

---
+++ src/test/java/com/edison/testJunit/oth/ThreadAndConcurrency/I_ThreadPool_ThreadPoolExecutor.java
@@ -0,0 +1,12 @@
+package com.edison.testJunit.oth.ThreadAndConcurrency;
+
+/**
+ * 创建线程池的推荐方法：ThreadPoolExecutor<p>
+ * 实现对一个数字数组分块计算合计值*/
+public class I_ThreadPool_ThreadPoolExecutor {
+
+	public static void main(String[] args) {
+
+	}
+
+}
