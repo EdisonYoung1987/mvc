1031413029c8aae25cd253c6c7e069af98f338e3
edisonyoung1987 <edison_young@aliyun.com>
2018-05-07 15:31:47 +0800

    新增流与文件的package

---
+++ src/test/java/com/edison/testJunit/oth/streamAndFile/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 流与文件<p>
+ * 1.访问文件和目录<br>
+ * 2.二进制格式读写<br>
+ * 3.文本格式读写<br>
+ * 4.序列化<br>
+ * 5.nio<br>
+ * 6.正则表达式<br>
+ */
+/**
+ * @author Edison
+ * 
+ */
+package com.edison.testJunit.oth.streamAndFile;
