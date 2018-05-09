f2cbe796aafeac5d21a83ddb53723af4f46d571d
edisonyoung1987 <edison_young@aliyun.com>
2018-05-09 16:26:31 +0800

    增加文件操作测试类

---
+++ src/test/java/com/edison/testJunit/oth/streamAndFile/E_FileOperation.java
@@ -0,0 +1,13 @@
+package com.edison.testJunit.oth.streamAndFile;
+
+import java.io.File;
+import java.nio.file.Path;
+
+/**文件操作*/
+public class E_FileOperation {
+
+	public static void main(String[] args) {
+		File file=new File("");
+	}
+
+}
