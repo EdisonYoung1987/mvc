3f9af68ce760919252c65189d22e6f6e833186f7
edisonyoung1987 <edison_young@aliyun.com>
2018-05-11 14:38:13 +0800

    提交NIO的BUFFER的使用，新增Filelock-未完成

--- src/test/java/com/edison/testJunit/oth/streamAndFile/G_Nio_Buffer.java
+++ src/test/java/com/edison/testJunit/oth/streamAndFile/G_Nio_Buffer.java
@@ -11,7 +11,7 @@ import java.nio.file.StandardOpenOption;
 
 
 public class G_Nio_Buffer {
-	private static final String FILE="D:\\工作\\其他\\传递外部文档.txt";
+	private static final String FILE="D:\\工作\\其他\\buff.txt";
 	private static final String FILE2="D:\\工作\\其他\\传递外部文档4.txt";
 
 	public static void main(String[] args) {
@@ -23,12 +23,19 @@ public class G_Nio_Buffer {
 														  StandardOpenOption.TRUNCATE_EXISTING,
 					                                      StandardOpenOption.WRITE);//计算校验和，只能read
 			//获取映射缓冲区buffer,缓冲区有三种模式 只读-读写(所有修改都会在某一时刻写入文件)-PRIVATE-读写(不写入文件)
-			ByteBuffer bf=ByteBuffer.allocate(200);
-			int j=0;
+			ByteBuffer bf=ByteBuffer.allocate(4);
+			
+			System.out.println("分配空间之后read()之前，buff的指针位置="+bf.position()+" 界限 "+bf.limit()+"\r\n");
 			while(channel.read(bf)!=-1){
-				bf.flip(); //要读的时候需要切换到读模式，且将指针指向0,否则下一步write什么都写不了(实际上随机写了一段进去。。)
+				System.out.println("read()之后，buff[4]的指针位置="+bf.position()+" 界限 "+bf.limit());
+				//flip是翻转读写模式，且将指针指向0,否则下一步write什么都写不了
+				bf.flip(); //如果最后一次读取内容长度len小于容量capacity,写入的内容是buf[len]->buf[capacity]的内容
+				System.out.println("read()之后执行flip()，buff[4]的指针位置="+bf.position()+" 界限 "+bf.limit());
 				channel2.write(bf);
-				bf.clear();//读完了要进行clear，以便下一次写，否则read(bf)不会改变buf的内容，channel一个byte都没读，死循环
+				System.out.println("write()写入channel之后，buff[4]的指针位置="+bf.position()+" 界限 "+bf.limit());
+				bf.clear();//读完了要重置指针，以便下一次写，否则read(bf)不会改变buf的内容，channel一个byte都没读，死循环
+				System.out.println("write()写入channel后执行clear(),buff[4]的指针位置="+bf.position()+" 界限 "+bf.limit());
+				System.out.println();
 			}
 			channel.close();
 			channel2.close();
---
+++ src/test/java/com/edison/testJunit/oth/streamAndFile/H_Nio_FileLock.java
@@ -0,0 +1,10 @@
+package com.edison.testJunit.oth.streamAndFile;
+
+/**给文件分段加锁*/
+public class H_Nio_FileLock {
+
+	public static void main(String[] args) {
+		FileChannel 
+	}
+
+}
