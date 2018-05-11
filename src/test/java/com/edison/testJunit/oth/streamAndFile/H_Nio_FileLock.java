3bd420a95d58707da2e8f44799382ede382c53f8
edisonyoung1987 <edison_young@aliyun.com>
2018-05-11 16:18:28 +0800

    提交文件锁FileLock-未完成

--- src/test/java/com/edison/testJunit/oth/streamAndFile/H_Nio_FileLock.java
+++ src/test/java/com/edison/testJunit/oth/streamAndFile/H_Nio_FileLock.java
@@ -1,10 +1,24 @@
 package com.edison.testJunit.oth.streamAndFile;
 
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**给文件分段加锁*/
 public class H_Nio_FileLock {
-
+	private static final String FILE="D:\\工作\\其他\\buff.txt";
 	public static void main(String[] args) {
-		FileChannel 
+		try{
+			Path path=Paths.get(FILE);
+			FileChannel filechannel=FileChannel.open(path,  StandardOpenOption.WRITE,StandardOpenOption.READ);
+			FileLock filelock=filechannel.lock(0, 4, false);
+			ss
+			filechannel.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
