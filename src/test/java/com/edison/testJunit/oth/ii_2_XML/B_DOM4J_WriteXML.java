ae384ee4f98c3af2b0bd337e11eb9695af853e81
edisonyoung1987 <edison_young@aliyun.com>
2018-05-24 15:04:07 +0800

    新增xpath解析xml，新增写xml文件-未完成

---
+++ src/test/java/com/edison/testJunit/oth/ii_2_XML/B_DOM4J_WriteXML.java
@@ -0,0 +1,11 @@
+package com.edison.testJunit.oth.ii_2_XML;
+
+/**写一个xml文件*/
+public class B_DOM4J_WriteXML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
