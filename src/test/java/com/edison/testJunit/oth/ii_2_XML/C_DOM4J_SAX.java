90903bae24616699de5c356776b5ac9c91a67233
edisonyoung1987 <edison_young@aliyun.com>
2018-05-24 16:12:43 +0800

    新增DOM4J+SAX解析xml文件-未完成

---
+++ src/test/java/com/edison/testJunit/oth/ii_2_XML/C_DOM4J_SAX.java
@@ -0,0 +1,11 @@
+package com.edison.testJunit.oth.ii_2_XML;
+
+/**使用DOM4J+SAX 即边读边解析，而不是一次加载到内存中，缓解内存压力*/
+public class C_DOM4J_SAX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
