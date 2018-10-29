47573abb5ee42098c46119e58e19b26ab149af42
EdisonYoung1987 <edison_young@aliyun.com>
2018-10-29 11:33:17 +0800

    增加几个空类

--- src/test/java/com/edison/testJunit/oth/zookeeper/base/ZookeeperUtil.java
+++ src/test/java/com/edison/testJunit/oth/zookeeper/base/ZookeeperUtil.java
@@ -1,5 +1,28 @@
 package com.edison.testJunit.oth.zookeeper.base;
 
-public class ZookeeperUtil {
+import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.zookeeper.ZooKeeper;
+
+
+/**
+ * 主要是zk连接的初始化工作等*/
+public class ZookeeperUtil {
+	private static ZooKeeper  zk=null;
+	public static void initZk() throws IOException{
+		//通过配置文件获取zk地址列表，实际spring项目中可以通过注解
+		//@Value("${zookeeper_addrs}")
+		Properties property=new Properties();
+		property.load(ClassLoader.getSystemResourceAsStream("dataSource.properties"));
+		String addrs=property.getProperty("zookeeper_addrs");
+		
+		String[] addrList=addrs.split(",");
+		for(String addr:addrList){
+			System.out.println("地址:"+addr);
+			
+		}
+		zk=new Z
+		return true;
+	}
 }
