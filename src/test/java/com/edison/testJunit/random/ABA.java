4d442fc700a85908ea5d20ba828bd990485ab27b
edisonyoung1987 <edison_young@aliyun.com>
2019-02-20 15:20:38 +0800

    AtomicStampedReference原子类解决ABA的问题

---
+++ src/test/java/com/edison/testJunit/random/ABA.java
@@ -0,0 +1,74 @@
+package com.edison.testJunit.random;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**在有些情况，比如通过compareAndSet(field,expect,update)的成功失败来判断某对象是否被变动过，
+ * 这时，就会出现ABA的情况，如https://www.cnblogs.com/java20130722/p/3206742.html，而AtomicStampedReference
+ * 通过引入版本号，每次操作compareAndSet时都把版本号+1，这样，就可以避免ABA的问题*/
+public class ABA {
+        private static AtomicInteger atomicInt = new AtomicInteger(100);
+        private static AtomicStampedReference atomicStampedRef = new AtomicStampedReference(100, 0);
+
+        public static void main(String[] args) throws InterruptedException {
+                Thread intT1 = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                atomicInt.compareAndSet(100, 101);
+                                atomicInt.compareAndSet(101, 100);
+                        }
+                });
+
+                Thread intT2 = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                try {
+                                        TimeUnit.SECONDS.sleep(1);
+                                } catch (InterruptedException e) {
+                                }
+                                boolean c3 = atomicInt.compareAndSet(100, 101);
+                                System.out.println(c3); // true
+                        }
+                });
+
+                intT1.start();
+                intT2.start();
+                intT1.join();
+                intT2.join();
+
+                Thread refT1 = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                try {
+                                        TimeUnit.SECONDS.sleep(1);
+                                	} catch (InterruptedException e) {
+                                }
+                                int stamp=atomicStampedRef.getStamp();
+                                boolean res1= atomicStampedRef.compareAndSet(100, 101, stamp,stamp+1);
+                                
+                                stamp=atomicStampedRef.getStamp();
+                                boolean res2= atomicStampedRef.compareAndSet(101, 100, stamp,stamp+1);
+                                System.out.println("res "+res1+" "+res2);
+                        }
+                });
+
+                Thread refT2 = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int stamp = atomicStampedRef.getStamp();//这就是版本号
+                                try {
+                                        TimeUnit.SECONDS.sleep(2);
+                                } catch (InterruptedException e) {
+                                }
+                                //预期值100，需替换为101，因为这里加了版本号，而该线程在sleep(2)后，
+                                //先前的版本号已经被修改,所以这里会失败
+                                boolean c3 = atomicStampedRef.compareAndSet(100, 101, stamp, stamp + 1);
+                                System.out.println(c3); // false
+                        }
+                });
+
+                refT1.start();
+                refT2.start();
+        }
+}
