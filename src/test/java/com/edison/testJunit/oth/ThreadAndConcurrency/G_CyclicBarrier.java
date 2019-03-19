0aaddbe39f4a10f2a7a27f33cc8bca8f0991d432
edisonyoung1987 <edison_young@aliyun.com>
2019-03-19 16:25:17 +0800

    调代码

--- src/test/java/com/edison/testJunit/oth/ThreadAndConcurrency/G_CyclicBarrier.java
+++ src/test/java/com/edison/testJunit/oth/ThreadAndConcurrency/G_CyclicBarrier.java
@@ -19,7 +19,11 @@ public class G_CyclicBarrier {
 		G_CyclicBarrier gcb=new G_CyclicBarrier();
 		MyRunnable_WriteFile wf=gcb.new MyRunnable_WriteFile();
 		for(int i=0;i<5;i++) {
-			new Thread(wf).start();
+			Thread thread=new Thread(wf);
+			thread.setName(""+i);
+			thread.start();
+			if(i==3)
+				thread.interrupt();
 		}
 
 	}
@@ -29,27 +33,41 @@ public class G_CyclicBarrier {
 		public void run() {
 			String fileName=Thread.currentThread().getName();
 			File file=new File(PATH+fileName);
-			if(!file.exists()) {
-				try {
-					file.createNewFile();
-					while(true) {
-						try {
-							cyclicBarrier.await(); //到达屏障
-							break;
-						} catch (InterruptedException e) {//这些异常都需要重置屏障，然后重新await
-							e.printStackTrace();
-							cyclicBarrier.reset();
-						} catch (BrokenBarrierException e) {
-							e.printStackTrace();
-							cyclicBarrier.reset();
-						}
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			if(file.exists()) {
+				file.delete();
 			}
 			
+			try {
+				file.createNewFile();
+//				这里有问题
+				while(true) {
+					try {
+						System.out.println("线程"+Thread.currentThread().getName()+"进入await()");
+						cyclicBarrier.await(); //到达屏障
+						break;
+					} catch (InterruptedException e) {//这些异常都需要重置屏障，然后重新await
+//						e.printStackTrace();
+//						cyclicBarrier.reset(); //直接这样写的话就会被reset多次，不好
+						System.out.println("线程"+Thread.currentThread().getName()+"收到中断，"
+								+ "执行reset后重新await");
+						if(cyclicBarrier.isBroken()) {
+							synchronized (cyclicBarrier) {
+								if(cyclicBarrier.isBroken()) {
+									cyclicBarrier.reset(); //保证只被一个线程执行一次
+								}
+							}
+						}
+					} catch (BrokenBarrierException e) {//该异常两个可能: await时被中断、await的信号是broken的
+						System.out.println("线程"+Thread.currentThread().getName()+"收到异常Broken,"
+								+ "重新await()");
+//						e.printStackTrace();
+//						cyclicBarrier.reset();
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
