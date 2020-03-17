8902f7d926ae1cec2445b654cda0d0044ae8b428
edisonyoung1987 <edison_young@aliyun.com>
2020-03-17 17:31:08 +0800

    增加递归测试案例

--- src/test/java/com/edison/testJunit/oth/digui/Tree_toLinkedList.java
+++ src/test/java/com/edison/testJunit/oth/digui/Tree_toLinkedList.java
@@ -40,7 +40,46 @@ public class Tree_toLinkedList {
             val = x;
         }
     }
-    public TreeNode convertBiNode(TreeNode root) {
+    /**返回的左节点作为头结点，右节点为尾节点*/
+    public static  TreeNode convertBiNode(TreeNode root) {
+        TreeNode res=new TreeNode(-1);
+        res.left=res.right=null;//头结点和尾节点
+        TreeNode leftNode,rightNode;
 
+        if(root.left==null) {//左节点为空
+            res.left=root;
+        }else{//左节点不为空
+            leftNode=convertBiNode(root.left);
+            res.left=leftNode.left; //左边链的头结点作为整个返回的头结点
+            leftNode.right.right=root;//左边链的尾节点连接root节点
+            res.right=root; //root作为整个返回的尾节点
+        }
+
+        if(root.right==null) {
+            //不处理
+        }else {
+            rightNode=convertBiNode(root.left);
+            root.right=rightNode.left;//root重新连接右节点链表
+            res.right=rightNode.right;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n11=new TreeNode(1);//第一层为5的节点
+        TreeNode n22=new TreeNode(2);
+        TreeNode n25=new TreeNode(5);
+        TreeNode n33=new TreeNode(3);//第三次第一个为1的节点
+        TreeNode n34=new TreeNode(4);//第三次第二个为1的节点
+        TreeNode n36=new TreeNode(6);
+
+        n11.left=n22;n11.right=n25;
+        n22.left=n33;n22.right=n34;
+        n25.right=n36;
+
+        TreeNode newTreeNode=convertBiNode(n11);
+        while(newTreeNode.right!=null)
+            System.out.println(newTreeNode.right.val);
     }
 }
